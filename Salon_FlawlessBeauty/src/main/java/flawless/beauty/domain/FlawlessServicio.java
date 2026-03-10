7bc769fd08fe2e598b2b9830c6e8ae88712b2a75
aulate2001 <albertoulate113@gmail.com>
2026-03-10 17:48:19 -0600

    Domain - "FlawlessServicio" added

--- Salon_FlawlessBeauty/src/main/java/flawless/beauty/domain/FlawlessServicio.java
+++ Salon_FlawlessBeauty/src/main/java/flawless/beauty/domain/FlawlessServicio.java
@@ -10,13 +10,37 @@ package flawless.beauty.domain;
  */
 
 // Esta clase debe hacer lo siguiente:
-
 // Representar la entidad Servicio dentro del sistema.
 // Esta clase se conecta con la tabla servicio de la base de datos.
 // Contiene la información de los servicios que ofrece el salón.
 // Define atributos como id, nombre, descripción, precio y categoría.
 // Permite mostrar los servicios disponibles para que el usuario pueda agendar una cita.
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import java.io.Serializable;
+import lombok.Data;
 
 public class FlawlessServicio {
-    
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 100)
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Size(max = 255)
+    private String descripcion;
+
+    @NotNull
+    private Double precio;
+
+    // relación con categoria
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private FlawlessCategoria categoria;
 }
