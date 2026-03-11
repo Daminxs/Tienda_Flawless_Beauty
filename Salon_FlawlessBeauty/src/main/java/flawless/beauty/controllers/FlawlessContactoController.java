2feadb6587fb194ff635762deb215f981d20276c
DanielaNavarro03 <danielanavarroo03@hotmail.com>
2026-03-11 14:49:42 -0600

    Contactopart1

--- Salon_FlawlessBeauty/src/main/java/flawless/beauty/controllers/FlawlessContactoController.java
+++ Salon_FlawlessBeauty/src/main/java/flawless/beauty/controllers/FlawlessContactoController.java
@@ -4,19 +4,26 @@
  */
 package flawless.beauty.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  *
  * Encargado para: Daniela Navarro
  */
 
 // Esta clase debe hacer lo siguiente o contener:
-
 // Tomando en cuenta lo hablado en el avance 1, de sus requerimientos y funciones.
 // Controlar el apartado de Contacto del sitio web.
 // Recibir las solicitudes del navegador cuando el usuario entra a la sección "Contacto".
 // Mostrar la página contacto.html ubicada en la carpeta templates.
 // Permitir mostrar información del salón como teléfono, correo o ubicación.
 
+@Controller
 public class FlawlessContactoController {
-
+    
+    @GetMapping("/contacto")
+    public String mostrarContacto() {
+        return "contacto";
+    }
 }
