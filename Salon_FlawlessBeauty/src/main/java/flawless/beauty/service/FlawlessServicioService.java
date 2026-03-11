29a5d1fb261bea5be08d921e2ad4ed6466cb323c
aulate2001 <albertoulate113@gmail.com>
2026-03-10 18:25:35 -0600

    Service - "FlawlessServicioService" added

--- Salon_FlawlessBeauty/src/main/java/flawless/beauty/service/FlawlessServicioService.java
+++ Salon_FlawlessBeauty/src/main/java/flawless/beauty/service/FlawlessServicioService.java
@@ -16,6 +16,38 @@ package flawless.beauty.service;
 // Utilizar el repository de Servicio para acceder a la base de datos.
 // Servir como intermediario entre el controller y el repository cuando se trabaja con servicios.
 
+import flawless.beauty.domain.FlawlessServicio;
+import flawless.beauty.repository.FlawlessServicioRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FlawlessServicioService {
+
+    private final FlawlessServicioRepository servicioRepository;
+
+    public FlawlessServicioService(FlawlessServicioRepository servicioRepository) {
+        this.servicioRepository = servicioRepository;
+    }
+
+    public List<FlawlessServicio> getServicios() {
+        return servicioRepository.findAll();
+    }
+
+    public FlawlessServicio getServicio(Long id) {
+        return servicioRepository.findById(id).orElse(null);
+    }
+
+    public void save(FlawlessServicio servicio) {
+        servicioRepository.save(servicio);
+    }
+
+    public void delete(Long id) {
+        servicioRepository.deleteById(id);
+    }
     
+    public List<FlawlessServicio> getServiciosPorCategoria(Long categoriaId) {
+        return servicioRepository.findByCategoriaId(categoriaId);
+    }
+
 }
