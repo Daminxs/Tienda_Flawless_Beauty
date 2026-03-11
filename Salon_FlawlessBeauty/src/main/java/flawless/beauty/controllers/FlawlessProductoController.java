96684192d45842d34b1c964ea16926b87c36df5a
aulate2001 <albertoulate113@gmail.com>
2026-03-10 19:42:41 -0600

    Controller - "FlawlessServicioController" and "FlawlessProductoController" added

--- Salon_FlawlessBeauty/src/main/java/flawless/beauty/controllers/FlawlessProductoController.java
+++ Salon_FlawlessBeauty/src/main/java/flawless/beauty/controllers/FlawlessProductoController.java
@@ -17,6 +17,44 @@ package flawless.beauty.controllers;
 // Permitir mostrar información de cada producto como nombre, descripción, precio e imagen.
 // Permitir que el usuario vea los productos y pueda seleccionar uno para realizar una reserva.
 
+import flawless.beauty.domain.FlawlessProducto;
+import flawless.beauty.service.FlawlessProductoService;
+import flawless.beauty.service.FlawlessCategoriaService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@Controller
 public class FlawlessProductoController {
-    
+
+    private final FlawlessProductoService productoService;
+    private final FlawlessCategoriaService categoriaService;
+
+    public FlawlessProductoController(
+            FlawlessProductoService productoService,
+            FlawlessCategoriaService categoriaService) {
+
+        this.productoService = productoService;
+        this.categoriaService = categoriaService;
+    }
+
+    @GetMapping("/salonproductos")
+    public String listado(@RequestParam(required = false) Long categoria, Model model) {
+
+        var categorias = categoriaService.getCategoriasProductos();
+
+        List<FlawlessProducto> productos;
+
+        if (categoria == null) {
+            productos = productoService.getProductos();
+        } else {
+            productos = productoService.getProductosPorCategoria(categoria);
+        }
+
+        model.addAttribute("categorias", categorias);
+        model.addAttribute("productos", productos);
+
+        return "salonproductos/listado";
+    }
 }
--- Salon_FlawlessBeauty/src/main/java/flawless/beauty/controllers/FlawlessServicioController.java
+++ Salon_FlawlessBeauty/src/main/java/flawless/beauty/controllers/FlawlessServicioController.java
@@ -17,6 +17,43 @@ package flawless.beauty.controllers;
 // Mostrar información de cada servicio como nombre, descripción y precio.
 // Permitir que el usuario pueda seleccionar un servicio si desea agendar una cita.
 
+import flawless.beauty.domain.FlawlessServicio;
+import flawless.beauty.service.FlawlessServicioService;
+import flawless.beauty.service.FlawlessCategoriaService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@Controller
 public class FlawlessServicioController {
+
+    private final FlawlessServicioService servicioService;
+    private final FlawlessCategoriaService categoriaService;
+
+    public FlawlessServicioController(FlawlessServicioService servicioService,
+                                      FlawlessCategoriaService categoriaService) {
+        this.servicioService = servicioService;
+        this.categoriaService = categoriaService;
+    }
+
+    @GetMapping("/salonservicios")
+    public String listado(@RequestParam(required = false) Long categoria, Model model) {
+
+        var categorias = categoriaService.getCategoriasServicios();
+
+        List<FlawlessServicio> servicios;
+
+        if (categoria == null) {
+            servicios = servicioService.getServicios();
+        } else {
+            servicios = servicioService.getServiciosPorCategoria(categoria);
+        }
+
+        model.addAttribute("categorias", categorias);
+        model.addAttribute("servicios", servicios);
+
+        return "salonservicios/listado";
+    }
     
 }
